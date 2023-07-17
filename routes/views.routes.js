e10eab9b18971639a148061cc62df57898370cb7
Lee <jrmun123@gmail.com>
2023-07-17 15:13:02 +0900

    feat: route 와 controller 연결

--- routes/views.routes.js
+++ routes/views.routes.js
@@ -4,6 +4,4 @@ const router = express.Router();
 const ViewController = require('../controllers/views.controller');
 const viewController = new ViewController();
 
-router.post('/restaurant/search', viewController.searchRestaurant);
-
 module.exports = router;
