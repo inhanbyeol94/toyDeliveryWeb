261e68d42e5303313bcaa5ca29c0039bfb37c045
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-18 15:18:31 +0900

    미인증 사용자 미들웨어 및 이메일 인증 validation 미들웨어 적용

--- routes/members.routes.js
+++ routes/members.routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const { allAuthMiddleware } = require('../middlewares/auth.middleware');
-const { signupValidation } = require('../middlewares/validations/members.validation');
+const { allAuthMiddleware, nonAuthMiddleware } = require('../middlewares/auth.middleware');
+const { signupValidation, loginValidation, isEmailValidation } = require('../middlewares/validations/members.validation');
 
 const MembersController = require('../controllers/members.controller');
 const membersController = new MembersController();
 
-router.post('/user/signup', signupValidation, membersController.signUp);
-router.post('/admin/signup', signupValidation, membersController.signUp);
-router.post('/signup/emailvalid', membersController.isEmailValid);
-router.post('/user/login', membersController.login);
-router.post('/admin/login', membersController.login);
+router.post('/user/signup', nonAuthMiddleware, signupValidation, membersController.signUp);
+router.post('/admin/signup', nonAuthMiddleware, signupValidation, membersController.signUp);
+router.post('/signup/emailvalid', nonAuthMiddleware, isEmailValidation, membersController.isEmailValid);
+router.post('/user/login', nonAuthMiddleware, loginValidation, membersController.login);
+router.post('/admin/login', nonAuthMiddleware, loginValidation, membersController.login);
 router.get('/logout', allAuthMiddleware, membersController.logout);
 // router.get('/member_info', membersController.getMember);
 // router.put('/member_info/:member_id', membersController.updateMember);
