a2597a3e5ce9c28a42129f3c20207bf91b8e5fc5
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-18 14:58:05 +0900

    사장님, 사용자 api구분하여 들어오는 경로에 따라 group이 결정될 수 있도록 분리

--- routes/members.routes.js
+++ routes/members.routes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const authMiddleware = require('../middlewares/auth.middleware');
+const { allAuthMiddleware } = require('../middlewares/auth.middleware');
 const { signupValidation } = require('../middlewares/validations/members.validation');
 
 const MembersController = require('../controllers/members.controller');
 const membersController = new MembersController();
 
-router.post('/signup', signupValidation, membersController.signUp);
+router.post('/user/signup', signupValidation, membersController.signUp);
+router.post('/admin/signup', signupValidation, membersController.signUp);
 router.post('/signup/emailvalid', membersController.isEmailValid);
-router.post('/login', membersController.login);
-router.get('/logout', authMiddleware, membersController.logout);
+router.post('/user/login', membersController.login);
+router.post('/admin/login', membersController.login);
+router.get('/logout', allAuthMiddleware, membersController.logout);
 // router.get('/member_info', membersController.getMember);
 // router.put('/member_info/:member_id', membersController.updateMember);
 // router.delete('/member_info/:member_id', membersController.deleteMember);
