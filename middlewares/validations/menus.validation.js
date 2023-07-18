f5f1028058298725e622a6e609d95a7f303c8dd1
Lee <jrmun123@gmail.com>
2023-07-18 20:11:29 +0900

    feat: 가게, 메뉴에 유효성 검사 추가

---
+++ middlewares/validations/menus.validation.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const { menu } = require('./message.json');
+
+const Menuvalidations = {
+    menuValidation: async (req, res, next) => {
+        const { name, price } = req.body;
+
+        const schema = Joi.object().keys({
+            name: Joi.string().empty().required().max(50).messages(menu.name),
+            price: Joi.number().empty().required().messages(menu.price),
+        });
+
+        try {
+            await schema.validateAsync({ name, price });
+        } catch (err) {
+            return res.status(412).json({ result: err.message });
+        }
+
+        next();
+    },
+};
+
+module.exports = Menuvalidations;
--- routes/restaurants.routes.js
+++ routes/restaurants.routes.js
@@ -3,13 +3,15 @@ const router = express.Router();
 
 const RestaurantController = require('../controllers/restaurants.controller');
 const restaurantController = new RestaurantController();
-const authMiddleware = require('../middlewares/auth.middleware');
+
+const { adminAuthMiddleware } = require('../middlewares/auth.middleware');
+const { restaurantValidation } = require('../middlewares/validations/restaurants.validation');
 
 router.get('/restaurant', restaurantController.getRestaurantList);
 router.get('/restaurant/:restaurant_id', restaurantController.getRestaurant);
-router.post('/restaurant', authMiddleware, restaurantController.createRestaurant);
-router.put('/restaurant/:restaurant_id', authMiddleware, restaurantController.updateRestaurant);
-router.delete('/restaurant/:restaurant_id', authMiddleware, restaurantController.deleteRestaurant);
+router.post('/restaurant', restaurantValidation, adminAuthMiddleware, restaurantController.createRestaurant);
+router.put('/restaurant/:restaurant_id', restaurantValidation, adminAuthMiddleware, restaurantController.updateRestaurant);
+router.delete('/restaurant/:restaurant_id', adminAuthMiddleware, restaurantController.deleteRestaurant);
 // router.post('/restaurant/search', restaurantController.searchRestaurant);
 
 module.exports = router;
