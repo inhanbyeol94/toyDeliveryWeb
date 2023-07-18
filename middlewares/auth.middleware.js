ec1241a1042346e1e7f982c3bb09169e7d0479e2
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-18 15:17:47 +0900

    미인증 사용자 전용 미들웨어 추가

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,17 @@
 const { Member } = require('../models');
 
 const authMiddlewares = {
+    nonAuthMiddleware: async (req, res, next) => {
+        try {
+            if (req.session.user) throw { code: 401, result: '로그인 상태에서는 작업할 수 없습니다.' };
+            next();
+        } catch (error) {
+            if (error.code) return res.status(401).json({ message: error.result });
+            console.log(error);
+            return res.status(500).json({ message: '오류가 발생하였습니다.' });
+        }
+    },
+
     allAuthMiddleware: async (req, res, next) => {
         try {
             if (!req.session.user) throw { code: 401, result: '로그인이 필요합니다.' };
