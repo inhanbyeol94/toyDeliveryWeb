7920a064c602e84aaf881d35f93e7d1c46baf443
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-18 14:58:43 +0900

    회원가입 validation 작업 완료

--- middlewares/validations/members.validation.js
+++ middlewares/validations/members.validation.js
@@ -1,20 +1,53 @@
 const Joi = require('joi');
-const { user } = require('./message.json');
+const { member } = require('./message.json');
 
 const Membervalidations = {
     signupValidation: async (req, res, next) => {
-        const body = req.body;
+        const { email, nickname, name, phone, address, password, confirmPassword, group } = req.body;
 
         const schema = Joi.object().keys({
-            email: Joi.string().required().empty().max(40).messages(user.email),
-            // .regex(/'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'/),
+            email: Joi.string()
+                .empty()
+                .max(40)
+                .regex(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i)
+                .required()
+                .messages(member.email),
+            nickname: Joi.string()
+                .empty()
+                .min(2)
+                .max(15)
+                .regex(/^[ㄱ-ㅎ가-힣a-zA-Z0-9]+$/)
+                .required()
+                .messages(member.nickname),
+            name: Joi.string()
+                .empty()
+                .required()
+                .min(2)
+                .max(10)
+                .regex(/^[가-힣a-zA-Z]+$/)
+                .messages(member.name),
+            phone: Joi.string()
+                .empty()
+                .required()
+                .min(11)
+                .max(15)
+                .regex(/^\d{2,3}-?\d{3,4}-?\d{4}$/)
+                .messages(member.phone),
+            address: Joi.string().empty().required().max(100).messages(member.address),
+            password: Joi.string()
+                .empty()
+                .min(8)
+                .max(20)
+                .regex(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])/)
+                .required()
+                .messages(member.password),
+            confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages(member.confirmPassword),
         });
 
         try {
-            await schema.validateAsync(body);
+            await schema.validateAsync({ email, nickname, name, phone, address, password, confirmPassword });
         } catch (err) {
-            return res.status(412).json({ err });
-            // return res.status(412).json({ message: err.message });
+            return res.status(412).json({ result: err.message });
         }
 
         next();
