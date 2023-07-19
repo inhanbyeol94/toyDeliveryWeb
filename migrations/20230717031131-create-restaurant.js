69d402ee4479f039a90a64aecf7691eee7a03acd
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-19 15:39:20 +0900

    restaurant 테이블 내 category column 추가 및 cart테이블에 restaurant_id 추가

--- migrations/20230717031131-create-restaurant.js
+++ migrations/20230717031131-create-restaurant.js
@@ -34,6 +34,10 @@ module.exports = {
                 allowNull: false,
                 type: Sequelize.TEXT,
             },
+            category: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+            },
             image: {
                 allowNull: true,
                 type: Sequelize.STRING,
