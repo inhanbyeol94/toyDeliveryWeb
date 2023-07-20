7f4851d43b6690ef6e70ea13a5e42089a3ba7b6f
Jean <etnpfj@gmail.com>
2023-07-20 23:43:02 +0900

    feat: order, cart 테스트 후 수정

--- services/orders.service.js
+++ services/orders.service.js
@@ -5,59 +5,80 @@ class OrderService {
     orderRepository = new OrderRepository();
     pointRepository = new PointRepository();
     memberRepository = new MemberRepository();
-    orderCart = async (cart_id, user) => {
-        const total = this.calculatePrice(user.member_id, cart_id);
-        if (total >= 0) {
-            throw { code: 404, message: '잔액이 부족합니다' };
+    orderCart = async ({ cart_id, member_id }) => {
+        try {
+            const total = await this.calculatePrice({ member_id, cart_id });
+            if (total <= 0) {
+                throw { code: 404, message: '잔액이 부족합니다' };
+            }
+            const pricePoint = await this.orderRepository.pricePoint({ cart_id });
+            const userInfo = await this.memberRepository.findOne({ member_id });
+            await this.pointRepository.createPoint(member_id, total, 0, '주문 비용 지불');
+            const order = await this.orderRepository.addOrder(pricePoint.restaurant_id, cart_id, userInfo.MemberInfos.member_info_id);
+
+            return order;
+        } catch (err) {
+            console.log(err);
         }
-        const pricePoint = await this.orderRepository.pricePoint(cart_id);
-        const userInfo = this.memberRepository.findOne(user.member_id);
-
-        await this.pointRepository.createPoint(user.member_id, total, 0, '주문 비용 지불'); //point사용 추가 수정필요
-        const order = await this.orderRepository.addOrder(pricePoint.restaurant_id, cart_id, userInfo.member_info_id);
-
-        return order;
     };
-    orderCheck = async (restaurant_id) => {
-        const order = await this.orderRepository.orderCheck(restaurant_id);
+    orderCheck = async ({ restaurant_id }) => {
+        const order = await this.orderRepository.orderCheck({ restaurant_id });
         if (!order) {
             throw { code: 404, message: '주문 목록을 불러오는중 오류가 생겼습니다' };
         }
         return order;
     };
-    orderUpdate = async (order_id, status, addTime, user) => {
-        const order = await this.orderRepository.findById(order_id);
-        if (!order) {
-            throw { code: 404, message: '해당 주문이 존재하지 않습니다' };
+    orderUpdate = async ({ order_id, status, addTime, user }) => {
+        try {
+            const order = await this.orderRepository.findById({ order_id });
+            if (!order) {
+                throw { code: 404, message: '해당 주문이 존재하지 않습니다' };
+            }
+            if (addTime === null) {
+                throw { code: 404, message: '예상 조리시간을 적어주세요' };
+            }
+            const arrival_at = this.calculateArriveTime(addTime);
+            const updatedOrder = await this.orderRepository.orderUpdate({ order_id, status, addTime, arrival_at });
+            if (updatedOrder[0] === 1) {
+                const orderCheck = await this.orderRepository.findById({ order_id });
+                if (orderCheck.status === 4) {
+                    const member_id = user.member_id;
+                    const cart_id = orderCheck.cart_id;
+                    const point = await this.calculatePrice({ member_id, cart_id });
+                    const point_status_code = 1;
+                    const reason = '판매 수익';
+                    await this.pointRepository.createPoint(member_id, point, point_status_code, reason);
+                    return { message: '배달완료처리 되었습니다' };
+                }
+                return { message: '반영되었습니다' };
+            }
+            throw { code: 404, message: '접수중 오류가 발생했습니다' };
+        } catch (err) {
+            console.log(err);
         }
-        if (addTime !== null) {
-            order.arriveTime = this.calculateArriveTime(addTime);
-        }
-        order.status = status;
-
-        const updatedOrder = await this.orderRepository.orderUpdate(order);
-        if (updatedOrder.status === 4) {
-            const price = this.calculatePrice(user.member_id, order.order_id);
-            await this.pointRepository.createPoint(user.member_id, price, 1, '판매 수익 입금');
-            return { message: '배달완료처리 되었습니다' };
-        }
-
-        return updatedOrder;
     };
 
     calculateArriveTime = (addTime) => {
-        if (addTime !== null) {
-            const currentDateTime = new Date();
-            const arrivalDateTime = new Date(currentDateTime.getTime() + addTime * 60000);
-            return arrivalDateTime;
-        } else {
-            return null;
-        }
+        const currentDateTime = new Date();
+        const arrivalDateTime = new Date(currentDateTime.getTime() + addTime * 60000);
+        return arrivalDateTime;
     };
-    calculatePrice = async function (member_id, cart_id) {
-        const userPoint = await this.pointRepository.userPoint(member_id);
-        const pricePoint = await this.orderRepository.pricePoint(cart_id);
-        return userPoint.point - pricePoint.count * pricePoint.price;
+    calculatePrice = async function ({ member_id, cart_id }) {
+        const findAllPoint = await this.pointRepository.findAllPoint(member_id);
+        let userPoint = 0;
+        for (let i in findAllPoint) {
+            if (i == 0) {
+                userPoint = findAllPoint[0].point;
+            } else if (findAllPoint[i].point_status_code == 0) {
+                userPoint = userPoint - findAllPoint[i].point;
+            } else if (findAllPoint[i].point_status_code == 1) {
+                userPoint = userPoint + findAllPoint[i].point;
+            }
+        }
+
+        const pricePoint = await this.orderRepository.pricePoint({ cart_id });
+        const price = userPoint - pricePoint['CartItems.count'] * pricePoint['CartItems.Menu.price'];
+        return price;
     };
 }
 
