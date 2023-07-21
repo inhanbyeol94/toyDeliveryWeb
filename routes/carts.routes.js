a796f943c065db72a627448989f3d87f77a98998
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-22 05:13:42 +0900

    삭제 및 카트 조회 추가

--- routes/carts.routes.js
+++ routes/carts.routes.js
@@ -4,8 +4,11 @@ const router = express.Router();
 const CartController = require('../controllers/carts.controller');
 const cartController = new CartController();
 
-router.post('/restaurant/:restaurant_id/cart', cartController.addCart);
-router.get('/restaurant/:restaurant_id/cart', cartController.getCart);
-router.delete('/restaurant/:restaurant_id/cart', cartController.deleteItem);
+const { allAuthMiddleware, nonAuthMiddleware } = require('../middlewares/api.auth.middleware');
+
+router.post('/restaurant/:restaurant_id/cart', allAuthMiddleware, cartController.addCart);
+router.get('/restaurant/:restaurant_id/cart', allAuthMiddleware, cartController.getCart);
+router.delete('/restaurant/:itemId/cart', allAuthMiddleware, cartController.deleteItem);
+router.get('/getRecentCart', allAuthMiddleware, cartController.getRecentCart);
 
 module.exports = router;
