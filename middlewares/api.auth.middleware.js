31f5892aa53ab482da6295e9a2aa8ce678a39fb0
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-22 07:06:23 +0900

    CustomError 재정의

--- middlewares/api.auth.middleware.js
+++ middlewares/api.auth.middleware.js
@@ -1,10 +1,10 @@
 const { Member } = require('../models');
-const customError = require('../errorClass');
+const { CustomError } = require('../customClass');
 
 const authMiddlewares = {
     nonAuthMiddleware: async (req, res, next) => {
         try {
-            if (req.session.user) throw new customError('로그인 상태에서는 작업할 수 없습니다', 401);
+            if (req.session.user) throw new CustomError('로그인 상태에서는 작업할 수 없습니다', 401);
             next();
         } catch (error) {
             if (error.status) return res.status(error.status).json({ message: error.message });
@@ -15,7 +15,7 @@ const authMiddlewares = {
 
     allAuthMiddleware: async (req, res, next) => {
         try {
-            if (!req.session.user) throw new customError('로그인이 필요합니다.', 401);
+            if (!req.session.user) throw new CustomError('로그인이 필요합니다.', 401);
             next();
         } catch (error) {
             if (error.status) return res.status(error.status).json({ message: error.message });
@@ -26,10 +26,10 @@ const authMiddlewares = {
 
     userAuthMiddleware: async (req, res, next) => {
         try {
-            if (!req.session.user) throw new customError('로그인이 필요합니다.', 401);
+            if (!req.session.user) throw new CustomError('로그인이 필요합니다.', 401);
             const findUser = await Member.findOne({ where: { member_id: req.session.user.member_id } });
 
-            if (findUser.group !== 0) throw new customError('고객 그룹만 접근이 가능합니다.', 401);
+            if (findUser.group !== 0) throw new CustomError('고객 그룹만 접근이 가능합니다.', 401);
 
             next();
         } catch (error) {
@@ -41,10 +41,10 @@ const authMiddlewares = {
 
     adminAuthMiddleware: async (req, res, next) => {
         try {
-            if (!req.session.user) throw new customError('로그인이 필요합니다.', 401);
+            if (!req.session.user) throw new CustomError('로그인이 필요합니다.', 401);
             const findUser = await Member.findOne({ where: { member_id: req.session.user.member_id } });
 
-            if (findUser.group !== 1) throw new customError('사장 그룹만 접근이 가능합니다.', 401);
+            if (findUser.group !== 1) throw new CustomError('사장 그룹만 접근이 가능합니다.', 401);
             next();
         } catch (error) {
             if (error.status) return res.status(error.status).json({ message: error.message });
