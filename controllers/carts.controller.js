4c6114ed657abfbd06555856c12996f6bc3d9614
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-22 05:13:11 +0900

    예외처리

--- controllers/carts.controller.js
+++ controllers/carts.controller.js
@@ -4,10 +4,9 @@ class CartsController {
     cartService = new CartService();
     addCart = async (req, res) => {
         try {
-            let { restaurant_id } = req.params;
+            const { restaurant_id } = req.params;
             const { menu_id, count } = req.body;
             const user = req.session.user;
-            restaurant_id = Number(restaurant_id);
             const data = await this.cartService.addCart({ restaurant_id, user, menu_id, count });
             res.status(200).json({ data });
         } catch (err) {
@@ -24,11 +23,30 @@ class CartsController {
     };
 
     deleteItem = async (req, res) => {
-        const { menu_id } = req.body;
-        const { restaurant_id } = req.params;
-        const member_id = req.session.user?.member_id;
-        const data = await this.cartService.deleteItem({ restaurant_id, member_id, menu_id });
-        res.status(200).json({ data });
+        try {
+            const { itemId } = req.params;
+            const { member_id } = req.session.user;
+            const { status, result } = await this.cartService.deleteItem({ itemId, member_id });
+            res.status(status).json({ result });
+        } catch (error) {
+            const { status, result } = error;
+            if (status) return res.status(error.status).json({ result });
+            console.error(error);
+            return res.status(500).json({ result: '오류가 발생하였습니다.' });
+        }
+    };
+
+    getRecentCart = async (req, res) => {
+        try {
+            const { member_id } = req.session.user;
+            const { status, result } = await this.cartService.getRecentCart({ member_id });
+            return res.status(status).json({ result });
+        } catch (error) {
+            const { status, result } = error;
+            if (status) return res.status(error.status).json({ result });
+            console.error(error);
+            return res.status(500).json({ result: '오류가 발생하였습니다.' });
+        }
     };
 }
 
