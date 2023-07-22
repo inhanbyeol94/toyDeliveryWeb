8a8ca82337b85b9bc75d839f0805299bc6c8b51c
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-22 18:52:22 +0900

    Code-refactoring

--- controllers/points.controller.js
+++ controllers/points.controller.js
@@ -5,48 +5,32 @@ class PointsController {
 
     /** 포인트 생성(추가및 차감)*/
     createPoint = async (req, res) => {
-        const url_member_id = req.params.member_id;
-        const { point, point_status_code, reason } = req.body;
-        const { member_id } = req.session.user;
-
         try {
+            const url_member_id = req.params.member_id;
+            const { point, point_status_code, reason } = req.body;
+            const { member_id } = req.session.user;
             const { status, message, result } = await this.pointService.postPoint(member_id, url_member_id, point, point_status_code, reason);
 
             res.status(status).json({ message, result });
         } catch (error) {
             if (error.status) return res.status(error.status).json({ message: error.message });
-
-            return res.status(500).json({ message: '포인트 작성에 실패했습니다.' });
+            console.error(error);
+            return res.status(500).json({ message: '오류가 발생하였습니다.' });
         }
     };
 
-    //해당 멤버 포인트 전체 조회
-    // getPoint = async (req, res) => {
-    //     const url_member_id = req.params.member_id;
-    //     const { member_id } = req.session.user;
-
-    //     try {
-    //         const points = await this.pointService.getPoint(member_id, url_member_id);
-
-    //         res.status(200).json({ points });
-    //     } catch (error) {
-    //         if (error.status) return res.status(error.status).json({ errorMessage: error.message });
-    //         return res.status(500).json({ errorMessage: '포인트 조회에 실패했습니다.' });
-    //     }
-    // };
-
     /** 해당 member 계산된 포인트 조회 */
     getPoint = async (req, res) => {
-        const url_member_id = req.params.member_id;
-        const { member_id } = req.session.user;
-
         try {
+            const url_member_id = req.params.member_id;
+            const { member_id } = req.session.user;
             const { status, message, result } = await this.pointService.calculation(member_id, url_member_id);
+
             res.status(status).json({ message, result });
         } catch (error) {
             if (error.status) return res.status(error.status).json({ message: error.message });
-
-            return res.status(500).json({ message: '포인트 조회에 실패했습니다.' });
+            console.error(error);
+            return res.status(500).json({ message: '오류가 발생하였습니다.' });
         }
     };
 }
